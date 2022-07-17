5031945c1f14e9580ed36f4b9c4ffe95219244dd
dsx137 <1419696375@qq.com>
2022-07-17 17:19:37 +0800

    textures

--- src/main/java/com/sunset/creativetab/TabLavaFishing.java
+++ src/main/java/com/sunset/creativetab/TabLavaFishing.java
@@ -1,23 +1,22 @@
 package com.sunset.creativetab;
 
 import com.sunset.util.Reference;
+import com.sunset.util.RegistryCollections.ItemCollection;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 public class TabLavaFishing extends CreativeModeTab
 {
-    public static final TabLavaFishing TAB_LAVA_FISHING =new TabLavaFishing(Reference.MOD_NAME);
-    public TabLavaFishing(String label)
-    {
+    public static final TabLavaFishing TAB_LAVA_FISHING = new TabLavaFishing(Reference.MOD_NAME);
+
+    public TabLavaFishing(String label) {
         super(label);
     }
 
     @NotNull
     @Override
-    public ItemStack makeIcon()
-    {
-        return new ItemStack(Items.APPLE);
+    public ItemStack makeIcon() {
+        return new ItemStack(ItemCollection.OBSIDIAN_FISHING_ROD);
     }
 }
