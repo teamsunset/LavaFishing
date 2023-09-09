6880b4e961ff0bb0aaaa99a085fb42b1928812e8
dsx137 <1419696375@qq.com>
2023-09-09 18:17:46 +0800

    fill recipe and loot table

--- src/main/java/com/sunset/lavafishing/LavaFishing.java
+++ src/main/java/com/sunset/lavafishing/LavaFishing.java
@@ -3,16 +3,14 @@ package com.sunset.lavafishing;
 import com.mojang.logging.LogUtils;
 import com.sunset.lavafishing.util.Reference;
 import com.sunset.lavafishing.util.RegistryCollection.*;
-import com.teammetallurgy.aquaculture.Aquaculture;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(Reference.MOD_ID)
-public class LavaFishing
-{
-    public static final Logger SG_LOGGER = LogUtils.getLogger();
+public class LavaFishing {
+    public static final Logger DEBUG_LOGGER = LogUtils.getLogger();
 
     public LavaFishing() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
