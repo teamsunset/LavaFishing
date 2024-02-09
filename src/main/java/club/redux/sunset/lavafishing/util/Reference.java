6f774f0d9a0eddc3dd6b851f264f2e0bad93261b
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-02-09 13:41:34 +0800

    change proj structure

---
+++ src/main/java/club/redux/sunset/lavafishing/util/Reference.java
@@ -0,0 +1,9 @@
+package club.redux.sunset.lavafishing.util;
+
+public class Reference
+{
+    public static final String MOD_ID = "lavafishing";
+    public static final String MOD_NAME = "Lava Fishing";
+
+    public static final String AQUACULTURE_MOD_ID = "aquaculture";
+}
--- src/main/java/com/sunset/lavafishing/util/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.sunset.lavafishing.util;
-
-public class Reference
-{
-    public static final String MOD_ID = "lavafishing";
-    public static final String MOD_NAME = "Lava Fishing";
-
-    public static final String AQUACULTURE_MOD_ID = "aquaculture";
-}
