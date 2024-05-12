81309d8a340b0466ed9140f5acc9f08ce9b73ae1
dsx137 <1419696375@qq.com>
2024-05-12 20:03:03 +0800

    fix armors' durability

--- src/main/java/club/redux/sunset/lavafishing/item/slingshot/ItemPromethiumSlingshot.kt
+++ src/main/java/club/redux/sunset/lavafishing/item/slingshot/ItemPromethiumSlingshot.kt
@@ -1,7 +1,6 @@
 package club.redux.sunset.lavafishing.item.slingshot
 
 import club.redux.sunset.lavafishing.misc.ModTiers
-import club.redux.sunset.lavafishing.util.isServerSide
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
@@ -14,18 +13,16 @@ class ItemPromethiumSlingshot : ItemSlingshot(
 
     override fun onUseTick(pLevel: Level, pLivingEntity: LivingEntity, pStack: ItemStack, pRemainingUseDuration: Int) {
         super.onUseTick(pLevel, pLivingEntity, pStack, pRemainingUseDuration)
-        if (pLivingEntity !is Player) return
+        if (pLivingEntity !is Player || pLevel.isClientSide) return
 
         val item = pStack.item as ItemPromethiumSlingshot
-        if (pLevel.isServerSide()) {
-            val duration = item.getUseDuration(pStack)
-            var overtime = duration - pRemainingUseDuration
-            overtime *= item.getChargeMultiplier(pStack)
-            if (overtime < 0) return
-            val timePower = getPowerForTime(overtime)
-            if (timePower == 1.0F) {
-                pLivingEntity.releaseUsingItem()
-            }
+        val duration = item.getUseDuration(pStack)
+        var overtime = duration - pRemainingUseDuration
+        overtime *= item.getChargeMultiplier(pStack)
+        if (overtime < 0) return
+        val timePower = getPowerForTime(overtime)
+        if (timePower == 1.0F) {
+            pLivingEntity.releaseUsingItem()
         }
     }
 }
--- src/main/java/club/redux/sunset/lavafishing/misc/ModArmorMaterials.kt
+++ src/main/java/club/redux/sunset/lavafishing/misc/ModArmorMaterials.kt
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundEvents
 import net.minecraft.util.StringRepresentable
 import net.minecraft.world.item.ArmorItem
 import net.minecraft.world.item.ArmorMaterial
+import net.minecraft.world.item.ArmorMaterials
 import net.minecraft.world.item.crafting.Ingredient
 import java.util.*
 
@@ -13,7 +14,6 @@ enum class ModArmorMaterials(
     private val materialName: String,
     private val durabilityMultiplier: Int,
     private val defenseForType: EnumMap<ArmorItem.Type, Int>,
-    private val durabilityForType: EnumMap<ArmorItem.Type, Int>,
     private val enchantmentValue: Int,
     private val sound: SoundEvent,
     private val toughness: Float,
@@ -23,27 +23,23 @@ enum class ModArmorMaterials(
 
     PROMETHIUM(
         "promethium",
-        38,
+        30,
         EnumMap<ArmorItem.Type, Int>(ArmorItem.Type::class.java).apply {
-            put(ArmorItem.Type.BOOTS, 4)
-            put(ArmorItem.Type.LEGGINGS, 7)
-            put(ArmorItem.Type.CHESTPLATE, 9)
             put(ArmorItem.Type.HELMET, 4)
-        },
-        EnumMap<ArmorItem.Type, Int>(ArmorItem.Type::class.java).apply {
-            put(ArmorItem.Type.BOOTS, 4)
-            put(ArmorItem.Type.LEGGINGS, 7)
             put(ArmorItem.Type.CHESTPLATE, 9)
-            put(ArmorItem.Type.HELMET, 4)
+            put(ArmorItem.Type.LEGGINGS, 7)
+            put(ArmorItem.Type.BOOTS, 4)
         },
         14,
-        SoundEvents.ARMOR_EQUIP_DIAMOND,
+        SoundEvents.ARMOR_EQUIP_NETHERITE,
         2.5f,
         0.1f,
         { Ingredient.of(ModItems.PROMETHIUM_INGOT.get()) }
     );
 
-    override fun getDurabilityForType(type: ArmorItem.Type): Int = durabilityForType[type]!! * this.durabilityMultiplier
+    override fun getDurabilityForType(type: ArmorItem.Type): Int =
+        ArmorMaterials.HEALTH_FUNCTION_FOR_TYPE[type]!! * this.durabilityMultiplier
+
     override fun getDefenseForType(type: ArmorItem.Type): Int = defenseForType[type]!!
     override fun getEnchantmentValue(): Int = this.enchantmentValue
     override fun getEquipSound(): SoundEvent = this.sound
