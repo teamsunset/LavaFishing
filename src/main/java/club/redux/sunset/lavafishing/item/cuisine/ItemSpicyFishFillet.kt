e48de539460bced3938dd99c1b2aa981c2d134b0
dsx137 <1419696375@qq.com>
2024-05-28 12:56:44 +0800

    textures

--- src/main/java/club/redux/sunset/lavafishing/item/cuisine/ItemSpicyFishFillet.kt
+++ src/main/java/club/redux/sunset/lavafishing/item/cuisine/ItemSpicyFishFillet.kt
@@ -2,21 +2,23 @@ package club.redux.sunset.lavafishing.item.cuisine
 
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.food.FoodProperties
-import net.minecraft.world.item.Item
+import net.minecraft.world.item.BowlFoodItem
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 import net.minecraft.world.level.Level
 
-class ItemSpicyFishFillet : Item(
+class ItemSpicyFishFillet : BowlFoodItem(
     Properties()
         .food(
             FoodProperties.Builder()
                 .nutrition(7)
-                .saturationMod(0.5f)
+                .saturationMod(0.8f)
                 .build()
         )
 ) {
     override fun finishUsingItem(pStack: ItemStack, pLevel: Level, pLivingEntity: LivingEntity): ItemStack {
         pLivingEntity.remainingFireTicks = 20
+        Items.MUSHROOM_STEW
         return super.finishUsingItem(pStack, pLevel, pLivingEntity)
     }
 }
