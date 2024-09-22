d92de7c29fbf05760dac5067d83613b69624d16a
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-09-22 17:46:02 +0800

    chore: models and tooltips

--- src/main/java/club/redux/sunset/lavafishing/jvm/ModClassLoader.kt
+++
@@ -1,7 +0,0 @@
-package club.redux.sunset.lavafishing.jvm
-
-class ModClassLoader : ClassLoader() {
-    override fun findClass(name: String?): Class<*> {
-        return super.findClass(name)
-    }
-}
