502d50c5c03e34349a7fda04f25a7a995e416e81
dsx137 <1419696375@qq.com>
2024-09-24 17:08:03 +0800

    chore: bat file

--- startForDev.bat
+++ startForDev.bat
@@ -2,22 +2,42 @@
 chcp 65001
 cls
 cd /d %~dp0
-echo 非常好脚本，使我的项目运行
-set isSetUser=
-set /p isSetUser="要设置用户名吗？ y/n: "
-set username=
-if "%isSetUser%"=="y" (
-    echo 设置用户名
-    set /p username="请输入用户名: "
+
+:main
+    call :output "非常好脚本，使我的项目运行"
+    call :output "开始运行项目 1/2……"
+    call :output
+    cmd /C gradlew runData
+    call :output
+    call :input "username" "设置用户名（回车跳过）: "
+    if "%username%"=="" (
+        call :output "不设置用户名"
+        call :output "开始运行项目 2/2……"
+        call :output
+        cmd /C gradlew runClient
+    ) else (
+        call :output "设置用户名为： %username%"
+        call :output "开始运行项目 2/2……"
+        call :output
+        cmd /C gradlew runClient --args="--username %username%"
+    )
+    exit /b
+
+:output
+set count=0
+for %%i in (%*) do (
+    set /a count+=1
+    if "%%~i"=="" (
+        echo.
+    ) else (
+        echo %%~i
+    )
 )
-echo 不设置用户名
-echo 开始运行项目……
-echo,
-if "%username%"=="" (
-    gradlew runClientAfterRunData
-) else (
-    gradlew runClientAfterRunData --args="--username %username%"
-)
+if %count%==0 echo.
+exit /b
 
-
-
+:input
+    more +1 < nul
+    set "%~1="
+    set /p "%~1=%2"
+    exit /b
