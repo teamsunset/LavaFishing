fd5dedafb883e2fdc22d68a9e4449cc524c47755
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-09-25 20:08:51 +0800

    chore: bat file

--- startForDev.bat
+++ startForDev.bat
@@ -5,6 +5,16 @@ cd /d %~dp0
 
 :main
     call :output "非常好脚本，使我的项目运行"
+    call :input "proxy" "设置代理（回车跳过）："
+    if "%proxy%"=="" (
+        call :output "不设置代理"
+    ) else (
+        call :output "设置代理为： %proxy%"
+        set http_proxy=%proxy%
+        set https_proxy=%proxy%
+        @REM cmd /C gradlew systemProp.http.proxyHost=%proxy% systemProp.http.proxyPort=1080 systemProp.https.proxyHost=%proxy% systemProp.https.proxyPort=1080
+    )
+
     call :output "开始运行项目 1/2……"
     call :output
     cmd /C gradlew runData
