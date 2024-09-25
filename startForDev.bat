7b64f49f369d0202b4b7a954c36808de1500d21c
dsx137 <1419696375@qq.com>
2024-09-25 20:44:06 +0800

    chore: bat file

--- startForDev.bat
+++ startForDev.bat
@@ -10,8 +10,8 @@ cd /d %~dp0
         call :output "不设置代理"
     ) else (
         call :output "设置代理为： %proxy%"
-        set http_proxy=%proxy%
-        set https_proxy=%proxy%
+        set "http_proxy=%proxy%"
+        set "https_proxy=%proxy%"
         @REM cmd /C gradlew systemProp.http.proxyHost=%proxy% systemProp.http.proxyPort=1080 systemProp.https.proxyHost=%proxy% systemProp.https.proxyPort=1080
     )
 
