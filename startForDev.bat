0feac3ec96418ced3ea98d7544f5f6fe330b5175
dsx137 <1419696375@qq.com>
2024-09-25 22:19:59 +0800

    chore: bat file remove space

--- startForDev.bat
+++ startForDev.bat
@@ -9,7 +9,7 @@ cd /d %~dp0
     if "%proxy%"=="" (
         call :output "不设置代理"
     ) else (
-        call :output "设置代理为： %proxy%"
+        call :output "设置代理为：%proxy%"
         set "http_proxy=%proxy%"
         set "https_proxy=%proxy%"
         @REM cmd /C gradlew systemProp.http.proxyHost=%proxy% systemProp.http.proxyPort=1080 systemProp.https.proxyHost=%proxy% systemProp.https.proxyPort=1080
