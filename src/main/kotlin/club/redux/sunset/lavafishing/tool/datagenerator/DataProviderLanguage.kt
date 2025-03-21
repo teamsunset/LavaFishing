2a1721055ab874c22a33ffadc198016bb63f00c6
dsx137 <1419696375@qq.com>
2025-03-22 06:56:04 +0800

    fix: bullet lang

--- src/main/kotlin/club/redux/sunset/lavafishing/tool/datagenerator/DataProviderLanguage.kt
+++ src/main/kotlin/club/redux/sunset/lavafishing/tool/datagenerator/DataProviderLanguage.kt
@@ -14,14 +14,25 @@ abstract class DataProviderLanguage(
     modId: String,
     private val locale: Locale,
 ) : LanguageProvider(packOutput, modId, locale.toString().lowercase()) {
-    protected fun choose(vararg pairs: Pair<Locale, String>): String {
+    protected fun match(vararg pairs: Pair<Locale, String>): String {
         return pairs.find { it.first == this.locale }?.second ?: throw IllegalArgumentException("Locale not found")
     }
 
-    protected fun Item.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
-    protected fun Block.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
-    protected fun EntityType<*>.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
-    protected fun String.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
-    protected fun ItemStack.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
-    protected fun MobEffect.addTranslation(vararg pairs: Pair<Locale, String>) = add(this, choose(*pairs))
+    protected fun Item.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
+
+    protected fun Block.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
+
+    protected fun EntityType<*>.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
+
+    protected fun String.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
+
+    protected fun ItemStack.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
+
+    protected fun MobEffect.addTranslation(vararg pairs: Pair<Locale, String>) =
+        add(this, this@DataProviderLanguage.match(*pairs))
 }
